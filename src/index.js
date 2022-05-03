10ec556fab1ed0e61c99448dc0e4b2ebb3e3e18a
Corey Schadeck <corey@schadeck.com>
2022-05-03 12:44:23 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,27 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
-//function
-const createNewTask = (e) =>{
-  e.preventDefault();
-  console.log('submit')
-  //grab the input
-  const newTaskDescription = document.getElementById('new-task-description')
-  console.log(newTaskDescription.value)
- //creat li's
-  const newLi = document.createElement('li')
-  console.log(newLi)
+  //function
+  const createNewTask = (e) => {
+    e.preventDefault();
+    //grab the input
+    const newTaskDescription = document.getElementById('new-task-description')
+    //creat li's
+    const newLi = document.createElement('li')
     //print input to li
-  newLi.textContent = newTaskDescription.value
-  //append
-  const ul = document.querySelector("#tasks")
-  console.log(ul)
-  ul.appendChild(newLi)
-  
-  e.target.reset();
-}
-  // grabb info from the DOM
-const form = document.getElementById(`create-task-form`)
+    newLi.textContent = newTaskDescription.value
+    //append
+    const ul = document.querySelector("#tasks")
+    ul.appendChild(newLi)
 
+    e.target.reset();
+  }
+  // grabb info from the DOM
+  const form = document.getElementById(`create-task-form`)
 
   //event listeners
-form.addEventListener('submit',createNewTask)
+  form.addEventListener('submit', createNewTask)
 });
